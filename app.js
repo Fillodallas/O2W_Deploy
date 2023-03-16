2237cae5fdce9ff654dd3fb634cb08b953b80d94
Fillodallas <filippo.dallas@gmail.com>
2023-03-16 16:26:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 // Allow cross-origin resource sharing
-app.use(cors());
-
+app.use(cors({
+  origin: "*",
+}));
 // Serve the index.html page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
